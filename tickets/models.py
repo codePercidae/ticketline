0247a022022614a17ec4a88307e2479f6a309204
Tommi Kiiski <kiiski.tommi@hotmail.com>
2026-03-23 12:09:12 +0200

    Event class and some hard coded data for it

--- tickets/models.py
+++ tickets/models.py
@@ -7,3 +7,12 @@ class User(models.Model):
     password = models.CharField(max_length=20)
     balance = models.IntegerField()
     admin = models.BooleanField()
+
+class Event(models.Model):
+    name = models.CharField()
+    price = models.IntegerField()
+    transactionfee = models.FloatField(db_default=2.5)
+    extrafee = models.FloatField(db_default=5.0)
+    extrafee2 = models.FloatField(db_default=4.5)
+    extrafee3 = models.FloatField(db_default=150)
+    extrafee4 = models.FloatField(db_default=0.5)
