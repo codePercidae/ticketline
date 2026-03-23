2027941cb486e242ef652d64a20ed4853e318baf
Tommi Kiiski <kiiski.tommi@hotmail.com>
2026-03-23 12:14:53 +0200

    Add functionality for admin

--- tickets/views.py
+++ tickets/views.py
@@ -1,19 +1,23 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-from .models import User
+from .models import User, Event
 
 
 # Create your views here.
 
 def index(request):
-    context = {"user" : False}
+    context = {"user" : False, "admin" : False}
     print(request.session.keys())
     if "user" in request.session:
         print("user")
         context["user"] = request.session["user"]
         context["username"] = request.session["username"]
         context["balance"] = request.session["balance"]
+        if request.session["admin"]:
+            context["admin"] = True
+    events = Event.objects.all()
+    context["events"] = events
     return render(request, "tickets/index.html", context=context)
 
 def login(request):
@@ -23,6 +27,7 @@ def logout(request):
     del request.session["user"]
     del request.session["username"]
     del request.session["balance"]
+    del request.session["admin"]
     return redirect("/")
 
 def auth(request):
@@ -34,5 +39,7 @@ def auth(request):
             request.session["user"] = u.id
             request.session["username"] = u.username
             request.session["balance"] = u.balance
+            request.session["admin"] = False
+        if u.admin:
+            request.session["admin"] = True
     return redirect("/")
-    
